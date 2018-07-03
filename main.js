67e69e7bea4f2345ced55b55c45e9f5b725513da
Ryan Palo <ryan@thepalos.com>
2018-07-03 09:08:47 -0700

    Commit entire repo at once like an animal.

---
+++ main.js
@@ -0,0 +1,49 @@
+var mymap = L.map('map').setView([40, -100], 4);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'example.key'
+}).addTo(mymap);
+
+// Until you put in your api key, this code won't work.
+console.error("Make sure the 'accessToken' on main.js:6 is your real api key.");
+
+var markers = [];
+var items = new Set;
+var selector = document.querySelector("#choice");
+
+d3.json('./data.json', function (data) {
+
+  data.forEach(function (datapoint) {
+    marker = L.marker(datapoint.coords,
+      {
+        item: datapoint.item
+      }
+    );
+    markers.push(marker);
+    marker.addTo(mymap);
+    items.add(datapoint.item);
+  });
+
+  items.forEach(function (item) {
+    const option = document.createElement('option');
+    option.innerHTML = item;
+    selector.appendChild(option);
+  })
+});
+
+selector.addEventListener('change', function () {
+  const selected = this.value;
+  markers.forEach(function (marker) {
+    if (marker.options.item == selected || selected == '') {
+      marker.setOpacity(1);
+    } else {
+      marker.setOpacity(0);
+    }
+  });
+});
+
+
+
+
